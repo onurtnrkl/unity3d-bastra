7b65cc331d3a26eab965c9132ac3a639dbe1257f
Onur Tanrikulu <onurtnrkl@gmail.com>
2017-09-18 00:52:28 +0300

    Player and Card Model added.

---
+++ Assets/Scripts/Models/Card.cs
@@ -0,0 +1,21 @@
+#region License
+/*================================================================
+Product:    Bastra
+Developer:  Onur Tanrıkulu
+Date:       18/09/2017 00:37
+
+Copyright (c) 2017 Onur Tanrikulu. All rights reserved.
+================================================================*/
+#endregion
+
+public sealed class Card
+{
+    public Rank Rank { get; private set; }
+    public Suit Suit { get; private set; }
+
+    public Card(Rank rank, Suit suit)
+    {
+        Rank = rank;
+        Suit = suit;
+    }
+}
---
+++ Assets/Scripts/Models/Player.cs
@@ -0,0 +1,21 @@
+#region License
+/*================================================================
+Product:    Bastra
+Developer:  Onur Tanrıkulu
+Date:       18/09/2017 00:47
+
+Copyright (c) 2017 Onur Tanrikulu. All rights reserved.
+================================================================*/
+#endregion
+
+using System.Collections.Generic;
+
+public sealed class Player
+{
+    public List<Card> Hand;
+
+    public Player()
+    {
+        Hand = new List<Card>();
+    }
+}
