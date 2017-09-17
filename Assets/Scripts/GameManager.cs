b3c3fa58e54e0ecce888ff93f16c4b6962cfed69
Onur Tanrikulu <onurtnrkl@gmail.com>
2017-09-18 00:16:01 +0300

    Game Manager added.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+#region License
+/*================================================================
+Product:    Bastra
+Developer:  Onur Tanrıkulu
+Date:       17/09/2017 23:51
+
+Copyright (c) 2017 Onur Tanrikulu. All rights reserved.
+================================================================*/
+#endregion
+
+using UnityEngine;
+
+public sealed class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField]
+    private Transform canvas;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+
+            Init();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void Init()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
