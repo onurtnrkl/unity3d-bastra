4042a2b4177da757ec8f6e8a7473a04853b8eb55
Onur Tanrikulu <onurtnrkl@gmail.com>
2017-09-18 00:34:24 +0300

    Models folder created for MVC pattern.
    Card Model enumaration types added.

---
+++ Assets/Scripts/Models/Rank.cs
@@ -0,0 +1,26 @@
+#region License
+/*================================================================
+Product:    Bastra
+Developer:  Onur Tanrıkulu
+Date:       18/09/2017 00:16
+
+Copyright (c) 2017 Onur Tanrikulu. All rights reserved.
+================================================================*/
+#endregion
+
+public enum Rank : byte
+{
+    A,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    K,
+    Q,
+    J
+};
---
+++ Assets/Scripts/Models/Suit.cs
@@ -0,0 +1,17 @@
+#region License
+/*================================================================
+Product:    Bastra
+Developer:  Onur Tanrıkulu
+Date:       18/09/2017 00:32
+
+Copyright (c) 2017 Onur Tanrikulu. All rights reserved.
+================================================================*/
+#endregion
+
+public enum Suit : byte
+{
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades
+}
