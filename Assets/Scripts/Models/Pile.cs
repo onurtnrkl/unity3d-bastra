527c0379d1330b76593cf3ec3b923db6cf1e44e8
Onur Tanrikulu <onurtnrkl@gmail.com>
2017-09-18 21:16:07 +0300

    SpriteManager added.

---
+++ Assets/Scripts/Models/Pile.cs
@@ -0,0 +1,36 @@
+#region License
+/*================================================================
+Product:    Bastra
+Developer:  Onur Tanrıkulu
+Date:       18/09/2017 20:03
+
+Copyright (c) 2017 Onur Tanrikulu. All rights reserved.
+================================================================*/
+#endregion
+
+using System.Collections.Generic;
+
+public sealed class Pile
+{
+    private List<Card> cards;
+    private bool isFirst;//is first pile? if is set active facedown card on pile area.
+
+    public Pile()
+    {
+        cards = new List<Card>();
+        isFirst = true;
+    }
+
+    public void AddCard(Card card)
+    {
+        cards.Add(card);
+    }
+
+    public Card[] TakeCards()
+    {
+        Card[] takenCards = cards.ToArray();
+        cards.Clear();
+
+        return takenCards;
+    }
+}
---
+++ Assets/Scripts/SpriteManager.cs
@@ -0,0 +1,50 @@
+#region License
+/*================================================================
+Product:    Bastra
+Developer:  Onur Tanrıkulu
+Date:       18/09/2017 20:38
+
+Copyright (c) 2017 Onur Tanrikulu. All rights reserved.
+================================================================*/
+#endregion
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class SpriteManager
+{
+    public static readonly SpriteManager Instance = new SpriteManager();
+
+    private Dictionary<string, Sprite> sprites;
+
+    private SpriteManager()
+    {
+        sprites = new Dictionary<string, Sprite>();
+
+        LoadAll();
+    }
+
+    private void LoadAll()
+    {
+        Sprite[] resources = Resources.LoadAll<Sprite>("Sprites");
+        int lenght = resources.Length;
+
+        for (int i = 0; i < lenght; i++)
+        {
+            Sprite sprite = resources[i];
+            string name = sprite.name;
+
+            sprites.Add(name, sprite);
+        }
+    }
+
+    /// <summary>
+    /// Returns sprite with name.
+    /// </summary>
+    /// <param name="name">Sprite name</param>
+    /// <returns></returns>
+    public Sprite GetSprite(string name)
+    {
+        return sprites[name];
+    }
+}
